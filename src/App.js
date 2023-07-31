5ab6bc6f8a0872177e3ebf2d15a64c42efcf7790
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-07-31 23:10:17 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import Activate from './Components/Main';
+
+
+
+function App() {
+  return (
+    <>
+   
+      <Activate />
+    </>
+  );
+}
+
+export default App;
